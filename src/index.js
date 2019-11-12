4fb8db69923813900679f0036c40574a1587d084
luca <l.lobacher@web.de>
2019-11-12 16:19:37 +0100

    Solves merge conflicts

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter } from 'react-router-dom'
-<<<<<<< HEAD
-import "semantic-ui-css/semantic.min.css"
-
-=======
 import 'semantic-ui-css/semantic.min.css'
->>>>>>> a355ba8e7b037ac12ad290f9d804a26d29125245
 
 ReactDOM.render(
   <BrowserRouter>
