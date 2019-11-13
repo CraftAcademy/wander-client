2dfe4da2d4254658c92be673d1dcc4f7d4890895
piavw <piavonwachenfelt@hotmail.com>
2019-11-13 15:40:18 +0100

    Creates fixture file and test for sadpath.

--- cypress/integration/userCanLogin.spec.js
+++ cypress/integration/userCanLogin.spec.js
@@ -20,10 +20,49 @@ describe('User can log in to application', () => {
       })
     cy.get('#login-form').within(() => {
       cy.get('#email-input').type('user@mail.com')
-      cy.get('#user-name').type('username')
       cy.get('#password-input').type('password')
     })
     cy.get('#submit-login-form').click()
     cy.get('#welcome-message').should('contain', 'Hello username')
+  })
+})
+
+describe('User can not log in to application', () => {
+  beforeEach(function() {
+  cy.server()
+  cy.route({
+    method: 'POST',
+    url: 'http://localhost:3000/auth/sign_in',
+    response: 'fixture:unsuccessful_user_login.json',
+    status: 401
+  })
+  cy.route({
+    method: 'GET',
+    url: 'http://localhost:3000/v1/trails',
+    response: 'fixture:user_can_view_list_of_trails.json'
+  })
+  cy.visit('http://localhost:3001'),
+  cy.get('#navbar')
+      .within(() => {
+        cy.get('#nav-login').click()
+      })
+  })
+
+  it('with wrong password', () => {
+    cy.get('#login-form').within(() => {
+      cy.get('#email-input').type('user@mail.com')
+      cy.get('#password-input').type('wrongpassword')
+    })
+    cy.get('#submit-login-form').click()
+    cy.get('#error-message').should('contain', 'Invalid login credentials. Please try again')
+  })
+
+  it('with wrong email', () => {
+    cy.get('#login-form').within(() => {
+      cy.get('#email-input').type('wrong@mail.com')
+      cy.get('#password-input').type('password')
+    })
+    cy.get('#submit-login-form').click()
+    cy.get('#error-message').should('contain', 'Invalid login credentials. Please try again')
   })
 })
