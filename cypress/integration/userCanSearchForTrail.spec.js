038c15ac0e8b47497f92418e8fb5a0e4889930e7
luca <l.lobacher@web.de>
2019-11-14 13:04:05 +0100

    Creates integration test for search functionality
    - Creates fixture file
    - Changes name of userCanCreateTrail integration file to match naming convention
    
    Co-Authored-By: Pia von Wachenfelt, <piavonwachenfelt@hotmail.com>

---
+++ cypress/integration/userCanSearchForTrail.spec.js
@@ -0,0 +1,32 @@
+describe('User can search for trail', () => {
+  it('successfully', () => {
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3000/v1/search',
+      response: 'fixture:successful_search.json',
+      status: 200
+    })
+    cy.get('#navbar')
+    .within(() => {
+      cy.get('#nav-search')
+        .within(() => {
+          cy.get('#search-input').type('Bollmora')
+          cy.get('#search-submit').click()
+        })
+    })
+    cy.get('#search-results').should('contain', 'Found adventures')
+    cy.get('#trail-list')
+    cy.get('#trail-list')
+    .within(() => {
+      cy.get('#trail_1')
+        .within(() => {
+          cy.get('#title_1').should('contain', 'Bollmora speed trail')
+          cy.get('#description_1').should('contain', 'A fast trail for fast people')
+          cy.get('#extra_1').should('contain', 'You are not prepared for the speed')
+          cy.get('#location_1').should('contain', 'Bollmora')
+          cy.get('#duration_1').should('contain', '10 minutes')
+          cy.get('#intensity_1').should('contain', '5')
+        })
+    })
+  })
+})
