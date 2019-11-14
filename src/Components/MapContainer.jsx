110c44bcba13fa0d1994d041af871426e375d56b
David_Izosimov <David.Izosimov@gmail.com>
2019-11-14 14:49:06 +0100

    Adds map function for trails data to separate  markers in MapContainer component.
    Co-Authored-By: Jonas Masharqa, <jonas.masharqa@gmail.com>

--- src/Components/MapContainer.jsx
+++ src/Components/MapContainer.jsx
@@ -1,15 +1,45 @@
-import { Map, GoogleApiWrapper } from 'google-maps-react'
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react'
 import React, { Component } from 'react'
 import { Container } from 'semantic-ui-react'
+import { getTrails } from '../Modules/trailsData'
 
 class MapContainer extends Component {
+  state = {
+    trails: [],
+    errorMessage: null
+  }
+
+  async componentDidMount() {
+    let response = await getTrails()
+    if (response.error_message) {
+      this.setState({
+        errorMessage: response.error_message
+      })
+    } else {
+      this.setState({
+        trails: response
+      })
+    }
+  }
+
   render() {
+    let trailsData = this.state.trails
     return (
       <Container>
       <Map 
         google={this.props.google} 
         zoom={14}
-      />
+      >
+      {trailsData.map(trail => {
+        return(
+          <Marker 
+            id={`trail_${trail.id}`}
+            title={trail.title}
+            position={{lat: trail.latitude, lng: trail.longitude}}
+          />
+        )
+      })}
+      </Map>
       </Container>
     )
   }
