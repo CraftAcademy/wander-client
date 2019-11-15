b4594044fe5de1ea39f0dab168e8e634946e7e76
jonas <jonas.masharqa@gmail.com>
2019-11-15 21:25:04 +0100

    Adds signInWrapper and redux resolution in package.json.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Route, Router, Redirect } from 'react-router-dom'
+import { Route, Router } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import { connect } from 'react-redux'
+import { generateRequireSignInWrapper } from 'redux-token-auth'
 import LandingPage from './Components/LandingPage'
 import SpecificTrail from './Components/SpecificTrail'
 import CreateTrail from './Components/CreateTrail'
@@ -12,25 +13,25 @@ import MapContainer from './Components/MapContainer';
 import SearchResults from './Components/SearchResults'
 import ProfilePage from './Components/ProfilePage'
 
+const requireSignIn = generateRequireSignInWrapper({
+  redirectPathIfNotSignedIn: '/login',
+})
+
 const history = createBrowserHistory({})
 
-const App = ({ currentUser }) => {
+const App = () => {
   return (
     <Router history={history}>
       <>
         <Navbar />
         <Route exact path='/' component={LandingPage}/>
         <Route exact path='/trails/:id' component={SpecificTrail}/>
-        <Route exact path='/create' component={CreateTrail}>
-          {currentUser.isSignedIn ? <Redirect to='/create' /> : <Login />}
-        </Route>
+        <Route exact path='/create' component={requireSignIn(CreateTrail)}/>
         <Route exact path='/search' component={SearchResults}/>
         <Route exact path='/login' component={Login} />
         <Route exact path='/signup' component={SignUp}/>
-        <Route exact path='/map' component={MapContainer} />
-        <Route exact path='/user/:name' component={ProfilePage}>
-          {currentUser.isSignedIn ? <Redirect to='/user/:name' /> : <Login />}
-        </Route>
+        <Route exact path='/map' component={MapContainer}/>
+        <Route exact path='/user/:name' component={requireSignIn(ProfilePage)}/>
       </>
     </Router>
   )
