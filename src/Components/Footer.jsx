431610c393962528b1aee897fc47a39e09773972
jonas <jonas.masharqa@gmail.com>
2019-11-16 17:32:22 +0100

    Creates footer component with NavLink to about us page.

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Footer() {
+  return (
+    <>
+      <NavLink to='/about'>About us</NavLink>
+    </>
+  )
+}
+
+export default Footer
