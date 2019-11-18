7b4f25f87fdcefe432abe0e2d7154b06320b0b6c
luca <l.lobacher@web.de>
2019-11-18 11:30:51 +0100

    Adds user image to profile and styling

--- src/Components/ProfilePage.jsx
+++ src/Components/ProfilePage.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Container, Divider } from 'semantic-ui-react'
+import { Container, Divider, Grid, Header, Image } from 'semantic-ui-react'
 
 function ProfilePage({ currentUser }) {
   return (
     <>
       <Container>
-        <h1 id='user-name'>{currentUser.attributes.name}</h1>
-        <Divider />
-        <br></br>
-        <div>
-          <h2>Email:</h2>
-          <h3 id='user-email'>{currentUser.attributes.uid}</h3>
-        </div>
+        <Grid columns={2}>
+          <Grid.Row>
+            <Grid.Column width={5}>
+              <Image id='user-image' src={currentUser.attributes.image} size='small' circular />
+            </Grid.Column>
+            <Grid.Column>
+              <Header as='h2' id='user-name'>{currentUser.attributes.name}</Header>
+              <Divider />
+              <br></br>
+              <div>
+                <h2>Email:</h2>
+                <h3 id='user-email'>{currentUser.attributes.uid}</h3>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Container>
     </>
   )
