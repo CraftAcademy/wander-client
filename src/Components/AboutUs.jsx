0322da12fa00fcce21ae6fcc1b70141ef86e3dcc
jonas <jonas.masharqa@gmail.com>
2019-11-18 23:29:18 +0100

    Writes paragraph for wander about page, adds img of team wander.

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Container, Divider } from 'semantic-ui-react'
+import TeamPic from '../Images/teampic.jpg'
 
 const AboutUs = () => {
+  const teamPic = <img src={TeamPic} alt='Wander team pic'/>
   return (
     <>
       <Container textAlign='center' >
         <h1>Wander</h1>
         <Divider/>
-        <p>Wander was developed as a final project hoping to inspire people to get out and explore nature around the world! </p>
+        <p>Developed in November 2019, Wander is a platform built for nature and hiking lovers across the globe that share
+          a passion for adventure.</p>
+          <p>Wander let's you share your favorite trails with others but also let's you discover new ones!
+          Bookmark your favorie trails, like the ones you enjoyed, and comment what you thought about that specific trail you so
+          bravely conquered!
+        </p>
         <h3>Who are we?</h3>
         <p>
           We are an elite alien space force tasked to bring you the best trailfinding app currently available on earth.
@@ -15,6 +22,7 @@ const AboutUs = () => {
         </p>
         <div>
           <h1>Team Wander</h1>
+          {teamPic}
           <h3>Luca Lobacher</h3>
           <h3>Alex Saxena</h3>
           <h3>Jonas Masharqa</h3>
