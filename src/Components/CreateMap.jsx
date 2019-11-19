3510edb3a4db19f298de29695973514ff06dc887
David_Izosimov <David.Izosimov@gmail.com>
2019-11-19 20:12:10 +0100

    Adds Polyline for displaying trail of markers in CreateMap component.
    Co-Authored-By: Luca Lobacher <l.lobacher@web.de>

--- src/Components/CreateMap.jsx
+++ src/Components/CreateMap.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react'
+import { Map, GoogleApiWrapper, Marker, Polyline } from 'google-maps-react'
 
 class CreateMap extends Component {
   render() {
     let trailsData = this.props.coordinates
+    const triangleCoords = trailsData.map(trail => ({lat: trail.latitude, lng: trail.longitude}))
 
     const style = {
       width: '80%',
@@ -18,12 +19,17 @@ class CreateMap extends Component {
           google={this.props.google} 
           zoom={3}
           style={style}
-          center={{
+          initialCenter={{
             lat: 30.0131,
             lng: 10.0686
           }}
           onClick={this.props.mapClicked}
         >
+        <Polyline
+          path={triangleCoords}
+          strokeColor="#0000FF"
+          strokeOpacity={0.8}
+          strokeWeight={2} />  
           {trailsData.map(trail => {
           return(
             <Marker 
