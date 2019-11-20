e59b8ee8b3e804672675d7009fb1b9e0379baecb
David_Izosimov <David.Izosimov@gmail.com>
2019-11-20 14:32:43 +0100

    Modifies integration test and fixture file for Search component.
    Co-Authored-By: Luca Lobacher <l.lobacher@web.de>

--- cypress/integration/userCanSearchForTrail.spec.js
+++ cypress/integration/userCanSearchForTrail.spec.js
@@ -2,7 +2,7 @@ describe('User can search for trail', () => {
   it('successfully', () => {
     cy.route({
       method: 'GET',
-      url: 'http://localhost:3000/v1//search/?search=Bollmora',
+      url: 'http://localhost:3000/v1/trails/?search=Bollmora',
       response: 'fixture:successful_search.json',
       status: 200
     })
